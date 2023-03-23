a414338e790ad93766982bc1b991bab0870dd2ff
alucchini <antoine.lucchini@ynov.com>
2023-03-23 14:04:35 +0100

    Initial Commit

---
+++ NavigationProgressBarButton/NavigationProgressBarButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationProgressBarButtonApp.swift
+//  NavigationProgressBarButton
+//
+//  Created by Antoine Lucchini on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationProgressBarButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
